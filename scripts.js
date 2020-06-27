a58b4d9469ad4958df7d8f9f65e03475a2cb8222
GiorgiKeburia <57075219+GiorgiKeburia@users.noreply.github.com>
2020-06-27 13:29:44 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,41 @@
+// Element Choose Function
+const selectElement = (s) => document.querySelector(s);
+//Open the menu click
+selectElement(".open").addEventListener("click", () => {
+  selectElement(".nav-list").classList.add("active");
+});
+//Close the menu click
+selectElement(".close").addEventListener("click", () => {
+  selectElement(".nav-list").classList.remove("active");
+});
+//////////////////////////////
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function myFunction2() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
